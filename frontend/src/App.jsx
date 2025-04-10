aea35e8fca5625cfdd89d71499c6c38aa011d978
Nguyen H. K. Bui <nguyenbui1996@gmail.com>
2025-04-09 21:48:31 -0700

    implement track web traffic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import { useLocation } from "react-router-dom";
 import Portfolio from "./pages/Portfolio.jsx";
+import { useEffect } from "react";
+import { trackVisit } from "./api-clients/client.jsx";
+
 
 function App() {
+    const location = useLocation();
 
-  return (
-      <div>
-          <Portfolio />
-      </div>
-  )
+    useEffect(() => {
+        trackVisit()
+    }, [location.pathname]);
+
+    return (
+        <div>
+            <Portfolio/>
+        </div>
+    )
 }
 
 export default App
--- frontend/src/api-clients/client.jsx
+++ frontend/src/api-clients/client.jsx
@@ -1,12 +1,38 @@
 import axios from "axios";
 
-const API_BASE = "https://api.a-pani.com/v1/download";
+const API_BASE = "https://api.a-pani.com";
+const API_BASE_DEV = "http://localhost:5000"
 
 export const downloadResume = async () => {
     try {
-        const res = await axios.get(`${API_BASE}/resume`);
+        const res = await axios.get(`${API_BASE_DEV}/v1/download/resume`);
         return res.data;
     } catch (err) {
         console.error("Download failed: ", err);
+    }
+};
+
+export const trackVisit = async () => {
+    try {
+        const res = await axios.post(`${API_BASE_DEV}/graphql`, {
+            query: `
+            mutation {
+                trackVisit(
+                    path: "${window.location.pathname}",
+                    userAgent: "${navigator.userAgent}"
+                )
+            }
+            `
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        console.log("📦 GraphQL response:", res.data);
+        if (res.data.errors) {
+            console.error("❌ trackVisit error:", res.data.errors);
+        }
+    } catch (error) {
+        console.error("❌ trackVisit failed:", error);
     }
 };
